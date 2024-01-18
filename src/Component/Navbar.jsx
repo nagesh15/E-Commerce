a0596b80c7b9d327b6678eb28e0771119bc6746a
nagesh15 <nageshshenoy2001@gmail.com>
2024-01-18 13:06:45 +0530

    first commit

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+        <div className="menu-list">
+            <NavLink>Home</NavLink>
+            <NavLink>Contact</NavLink>
+            <
+        </div>
+        <div className="navbar-icons">
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
